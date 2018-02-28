1c1265e1422fb8949457df6d7ec93f74e123463b
unknown <tekumseh777@gmail.com>
2018-02-28 16:06:15 +0100

    Added ES6 support, production build and cleaned sass code

--- app/javascript/app.js
+++ app/javascript/app.js
@@ -1,8 +1,8 @@
 // elements
-let start = document.getElementById('start');
+const start = document.getElementById('start');
 // jquery to simplify fade in-out process
-let front = $('#front');
-let loader = $('#loader');
+const front = $('#front');
+const loader = $('#loader');
 
 // initial first question indentifier and score-table
 let n = 1;
@@ -14,17 +14,17 @@ const score = {
 // on load start screen is shown
 document.addEventListener('DOMContentLoaded', function() {
     // load total number of questions for reference later
-    fetch('http://localhost:3001/totalQuestions')
+    fetch('http://localhost:3001/meta')
         .then(response => response.json())
         .then(json => {
-            score.totalQuestions = parseInt(json[0], 10);
+            score.totalQuestions = parseInt(json.totalQuestions, 10);
             console.log(score.totalQuestions);
         });
 
     // listen on start quiz click
     start.addEventListener('click', function(e) {
         // fade out start screen and load first question
-        front.fadeOut(200);
+        front.fadeOut(20);
         loadQuestion(n);
     }, false);
 
@@ -38,26 +38,32 @@ function handleAnswer(value) {
     // if answer selected allow to proceed
     submit.removeAttribute('disabled');
     // proceed to next question
-    submit.addEventListener('click', (e) => {
-        // show loader while fetching new question
-        loader.fadeIn(10);
-        // if answer is true log-it
-        if (theAnswer === value) {
-            score.rightAnswers += 1;
-        }
-        // if last question is answered calculateResult if not load next question
-        if (n <= score.totalQuestions) {
-            loadQuestion(n);
-        } else {
-            loader.fadeOut(200);
-            calculateResult();
-        }
-    }, false);
+    submit.addEventListener('click', processAnswer, false);
+}
+
+function processAnswer(e) {
+    e.preventDefault();
+    // show loader while fetching new question
+    loader.fadeIn(10);
+    // if answer is true log-it
+    if (theAnswer === value) {
+        score.rightAnswers += 1;
+    }
+    // if last question is answered calculateResult if not load next question
+    if (n <= score.totalQuestions) {
+        loadQuestion(n);
+    } else {
+        calculateResult(() => {
+            loader.fadeOut(100);
+        });
+    }
+
 }
 
 // calculate result function
-function calculateResult() {
-    questionare.innerHTML = `<h1 class="final-score">Right Answers: ${score.rightAnswers}</h1>`
+function calculateResult(callback) {
+    questionare.innerHTML = `<h1 class="final-score">Right Answers: ${score.rightAnswers}/${score.totalQuestions}</h1>`;
+    callback();
 }
 
 // fetch new question, compile hbs and append it
@@ -72,11 +78,11 @@ function loadQuestion(questId) {
         .then(response => response.json())
         .then(json => {
             let question = json.question;
-            let answers = json.answers;
             let theAnswer = json.theAnswer;
+            let answers = json.answers;
             let html = template({ question, theAnswer, answers });
             questionare.innerHTML = html;
-            loader.fadeOut(60);
+            loader.fadeOut(100);
             n += 1;
         });
 }
--- gulpfile.js
+++ gulpfile.js
@@ -1,49 +1,113 @@
 // import modules
 const gulp = require('gulp');
 const sass  = require('gulp-sass');
-const plumber = require('gulp-plumber');
 const browserSync = require('browser-sync').create();
+const plumber = require('gulp-plumber');
 const notify = require('gulp-notify');
+const cleanCSS = require('gulp-clean-css');
+const rename = require('gulp-rename');
+const uglify = require('gulp-uglify');
+const browserify = require('gulp-browserify');
+const babel = require('gulp-babel');
+const del = require('del');
 
 // paths
-const sassSource = ['app/sass/**/*.scss'];
+const sources = {
+    sass: ['app/sass/**/*.scss'],
+    js: 'app/javascript/app.js',
+    css: 'app/css/app.css',
+    idx: 'app/index.html'
+};
 
 // browrser sync server init
-gulp.task('browserSync', function() {
-  browserSync.init({
-    server: {
-      baseDir: './app'
-    }
-  });
+gulp.task('browserSync', ['sass'], () => {
+    browserSync.init({
+        server: {
+            baseDir: './app'
+        }
+    });
 
-  gulp.watch(sassSource, ['sass']);
-  gulp.watch('app/*.html').on('change', browserSync.reload);
-  gulp.watch('app/javascript/*.js').on('change', browserSync.reload);
+    gulp.watch(sources.sass, ['sass']);
+    gulp.watch('app/*.html').on('change', browserSync.reload);
+    gulp.watch('app/javascript/*.js').on('change', browserSync.reload);
 });
 
+gulp.task('serve', ['build'], () => {
+    browserSync.init({
+        server: {
+            baseDir: './dist'
+        }
+    });
+
+    gulp.watch('dist/*.min.css', browserSync.reload);
+    gulp.watch('dist/*.html').on('change', browserSync.reload);
+    gulp.watch('dist/*.min.js', browserSync.reload);
+})
+
 // sass compile task
-gulp.task('sass', function() {
-  return gulp.src(sassSource)
-    .pipe(plumber({ errorHandler: function(err) {
+gulp.task('sass', () => {
+    return gulp.src(sources.sass)
+        .pipe(plumber())
+        .pipe(sass.sync({
+            outputStyle: 'expanded'
+        }).on('error', sass.logError))
+        .pipe(gulp.dest('app/css/'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+
+// js process to dist/
+gulp.task('js-minify:build', () => {
+    return gulp.src(sources.js)
+        .pipe(plumber({ errorHandler: function(err) {
             notify.onError({
                 title: "Gulp error in " + err.plugin,
                 message:  err.toString()
             })(err);
         }}))
-    .pipe(sass())
-    .pipe(gulp.dest('app/css/'))
-    .pipe(browserSync.reload({
-      stream: true
-    }))
+        .pipe(babel({
+            presets: ['env']
+        }))
+        .pipe(uglify())
+        .pipe(rename((path) => path.basename += '.min'))
+        .pipe(gulp.dest('dist/'))
+        .pipe(notify('Javascript files are processed and saved to dist/'))
+        .pipe(browserSync.stream())
+});
+
+// css process to dist/
+gulp.task('css:build', () => {
+    return gulp.src(sources.css)
+        .pipe(plumber({ errorHandler: function(err) {
+            notify.onError({
+                title: "Gulp error in " + err.plugin,
+                message:  err.toString()
+            })(err);
+        }}))
+        .pipe(cleanCSS({ compatibility: '*' }))
+        .pipe(rename((path) => path.basename += '.min'))
+        .pipe(gulp.dest('dist/'))
+        .pipe(notify('CSS files are processed and saved to dist/'))
+        .pipe(browserSync.stream())
+});
+
+// html process to dist/
+gulp.task('html:build', () => {
+    return gulp.src(sources.idx)
+        .pipe(plumber())
+        .pipe(gulp.dest('dist/'))
+        .pipe(notify('Html file are processed and saved to dist/'))
+        .pipe(browserSync.stream())
 });
 
 gulp.task('default', ['browserSync']);
+gulp.task('build', ['js-minify:build', 'css:build', 'html:build']);
 
-// watch files and reload browserSync
-// gulp.task('watch', ['browserSync', 'sass'], function() {
-//   gulp.watch(sassSource, ['sass']);
-//   gulp.watch('app/*.html', browserSync.reload);
-//   gulp.watch('app/javascript/**/*.js', browserSync.reload);
-// });
 
+gulp.task('clean:dist', () => {
+    return del(['dist']).then(paths => {
+        console.log('Files and folders are deleted in:\n', paths.join('\n'));
+    });
+});
 
